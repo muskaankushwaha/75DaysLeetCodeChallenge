890c2de9f2efcb8a3600a96ecb22e156d3d12bbc
Muskaan Kushwaha <muskaankushwaha123@gmail.com>
2026-04-29 22:58:25 +0530

    Time: 1 ms (99.9%), Space: 46.6 MB (36.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -2,45 +2,51 @@ class Solution {
     public boolean isValidSudoku(char[][] board) {
         for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
-                char ch = board[i][j];
-                if(ch == '.') continue;
-
-                if(!(box(board, ch, i, j) && row(board, ch, i) && col(board, ch, j)))
-                    return false;
+                char ch=board[i][j];
+                if(ch=='.')continue;
+                if(!(box(board,ch,i,j)&&row(board,ch,i)&&col(board,ch,j)))return false;
+            
             }
+
         }
         return true;
+        
     }
-
-    public boolean box(char[][] board, char ch, int row, int col){
-        int count = 0;
-
-        // FIX: correct starting index of 3x3 box
-        int r = (row / 3) * 3;
-        int c = (col / 3) * 3;
-
-        for(int i = r; i < r + 3; i++){
-            for(int j = c; j < c + 3; j++){
-                if(board[i][j] == ch) count++;
-            }
+     public boolean box(char[][]board,char ch,int row, int col){
+        int count=0;
+        row=row/3;
+        col=col/3;
+        int r=0;
+        int c=0;
+        if(row==1)r=3;
+        else if(row==2)r=6;
+        if(col==1)c=3;
+        else if(col==2)c=6;
+        for(int i=r; i<r+3; i++){
+            for(int j=c; j<c+3; j++){
+                if(board[i][j]==ch)count++;
+                }
         }
-
-        return count <= 1;
-    }
-
-    public boolean row(char[][] board, char ch, int row){
-        int count = 0;
+                if(count>1)return false;
+                return true;      
+                  
+        }
+       
+    
+    public boolean row(char[][]board,char ch,int row){
+        int count=0;
         for(int i=0; i<9; i++){
-            if(board[row][i] == ch) count++;
-        }
-        return count <= 1;
-    }
-
-    public boolean col(char[][] board, char ch, int col){
-        int count = 0;
+            if(board[row][i]==ch)count++;
+                }
+                if(count>1)return false;
+                return true;      
+                  }
+     public boolean col(char[][]board,char ch,int col){
+        int count=0;
         for(int i=0; i<9; i++){
-            if(board[i][col] == ch) count++;
-        }
-        return count <= 1;
-    }
+            if(board[i][col]==ch)count++;
+                }
+                if(count>1)return false;
+                return true;    
+                    }
 }
