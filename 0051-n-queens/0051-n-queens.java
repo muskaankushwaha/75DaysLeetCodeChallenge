fd75ed594aeab5a877d8631bbf309a3adbaddf63
Muskaan Kushwaha <muskaankushwaha123@gmail.com>
2026-05-17 00:55:30 +0530

    Time: 1 ms (99.91%), Space: 46.4 MB (91.09%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,31 +1,31 @@
 class Solution {
-    boolean isSafe(int row,int col,char[][]board,int n){
-        int r=row;
-        int c=col;
-        //upper Diaginal
-        while(row>=0&&col>=0){
-            if(board[row][col]=='Q')return false;
-            row--;
-            col--;
-        }
-        row=r;
-        col=c;
-        //left row
-         while(col>=0){
-            if(board[row][col]=='Q')return false;
-            col--;
-        }
-        row=r;
-        col=c;
-        //lower diagonal
-         while(row<n&&col>=0){
-            if(board[row][col]=='Q')return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
-    void solve(int col,char[][]board,List<List<String>> ans,int n ){
+    // boolean isSafe(int row,int col,char[][]board,int n){
+    //     int r=row;
+    //     int c=col;
+    //     //upper Diaginal
+    //     while(row>=0&&col>=0){
+    //         if(board[row][col]=='Q')return false;
+    //         row--;
+    //         col--;
+    //     }
+    //     row=r;
+    //     col=c;
+    //     //left row
+    //      while(col>=0){
+    //         if(board[row][col]=='Q')return false;
+    //         col--;
+    //     }
+    //     row=r;
+    //     col=c;
+    //     //lower diagonal
+    //      while(row<n&&col>=0){
+    //         if(board[row][col]=='Q')return false;
+    //         row++;
+    //         col--;
+    //     }
+    //     return true;
+    // }
+    void solve(int col,char[][]board,List<List<String>> ans,int n ,int[] leftRow,int[] upperDiagonal, int[] lowerDiagonal){
         if(col==n){
             List<String> temp=new ArrayList<>();
             for(int i=0; i<n; i++){
@@ -35,10 +35,16 @@ class Solution {
             return;
         }
         for(int row=0; row<n; row++){
-            if(isSafe(row,col,board,n)){
+            if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[(n-1)+(col-row)]==0){
                 board[row][col]='Q';
-                solve(col+1,board,ans,n);
+                leftRow[row]=1;
+                upperDiagonal[n-1+col-row]=1;
+                lowerDiagonal[row+col]=1;
+                solve(col+1,board,ans,n,leftRow,upperDiagonal,lowerDiagonal);
                 board[row][col]='.';
+                 leftRow[row]=0;
+                upperDiagonal[n-1+col-row]=0;
+                lowerDiagonal[row+col]=0;
             }
         }
     }
@@ -48,7 +54,10 @@ class Solution {
         for(int i=0; i<n; i++){
             Arrays.fill(board[i],'.');
         }
-        solve(0,board,ans,n);
+        int [] leftRow=new int[n];
+        int[] upperDiagonal=new int[2*n-1];
+         int[] lowerDiagonal=new int[2*n-1];
+        solve(0,board,ans,n,leftRow,upperDiagonal,lowerDiagonal);
         return ans;
     }
 }
