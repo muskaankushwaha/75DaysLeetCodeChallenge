fe3aafe1d211eed1884ed237d8d945605bf09c65
Muskaan Kushwaha <muskaankushwaha123@gmail.com>
2026-05-22 22:54:39 +0530

    Time: 2737 ms (5.31%), Space: 57.5 MB (6.27%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class KthLargest {
+     List<Integer> list;
+     int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+    }
+    public int add(int val) {
+        list.add(val);
+        Collections.sort(list);
+        return list.get(list.size() - k);
+    }
+}
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
